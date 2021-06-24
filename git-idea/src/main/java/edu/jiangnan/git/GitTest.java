0a3e6314fbf09fb507793ec21445003ac9164955
Taohao <2291625396@qq.com>
2021-06-24 11:55:31 +0800

    IDEA 4rd git commit hot-fix

--- git-idea/src/main/java/edu/jiangnan/git/GitTest.java
+++ git-idea/src/main/java/edu/jiangnan/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello World!");
         System.out.println("Hello Java!");
+        System.out.println("Hello Jiangnan");
     }
 
 }
